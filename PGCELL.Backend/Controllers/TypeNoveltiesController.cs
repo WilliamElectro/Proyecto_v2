33fca5e515d03ead40a4642ecd3d4d58c0dcb59f
wbohorquez <wi-lli-am-3@hotmail.com>
2023-11-21 02:19:44 -0500

    Se añaden Controladores y Unidades de trabajo

---
+++ PGCELL.Backend/Controllers/TypeNoveltiesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PGCELL.Backend.UnitsOfWork;
+using PGCELL.Shared.DTOs;
+using PGCELL.Shared.Entites;
+
+namespace PGCELL.Backend.Controllers
+{
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/[controller]")]
+    public class TypeNoveltiesController : GenericController<TypeNovelty>
+    {
+        private readonly ITypeNoveltyUnitOfWork _typeNoveltyUnitOfWork;
+
+        public TypeNoveltiesController(IGenericUnitOfWork<TypeNovelty> unit, ITypeNoveltyUnitOfWork typeNoveltyUnitOfWork) : base(unit)
+        {
+            _typeNoveltyUnitOfWork = typeNoveltyUnitOfWork;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("combo")]
+        public async Task<IActionResult> GetComboAsync()
+        {
+            IEnumerable<TypeNovelty> combo = await _typeNoveltyUnitOfWork.GetComboAsync();
+            return Ok(combo);
+        }
+
+        [HttpGet]
+        public override async Task<IActionResult> GetAsync([FromQuery] PaginationDTO pagination)
+        {
+            var response = await _typeNoveltyUnitOfWork.GetAsync(pagination);
+            if (response.WasSuccess)
+            {
+                return Ok(response.Result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("totalPages")]
+        public override async Task<IActionResult> GetPagesAsync([FromQuery] PaginationDTO pagination)
+        {
+            var action = await _typeNoveltyUnitOfWork.GetTotalPagesAsync(pagination);
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return BadRequest();
+        }
+    }
+}
