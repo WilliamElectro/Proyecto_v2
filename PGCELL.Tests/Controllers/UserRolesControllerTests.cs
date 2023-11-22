a7409f9b1dab9507023ece21374d6e665683029a
Sduran <sergioaduran93@gmail.com>
2023-11-22 14:00:03 -0500

    Test Unit UserRol Controller

---
+++ PGCELL.Tests/Controllers/UserRolesControllerTests.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PGCELL.Backend.Controllers;
+using PGCELL.Backend.UnitsOfWork;
+using PGCELL.Shared.DTOs;
+using PGCELL.Shared.Entites;
+using PGCELL.Shared.Responses;
+
+namespace PGCELL.Tests.Controllers
+{
+    [TestClass]
+    public class UserRolesControllerTests
+    {
+        private Mock<IGenericUnitOfWork<UserRol>> _mockGenericUnitOfWork = null!;
+        private Mock<IUserRoleUnitOfWork> _mockUserRolUnitOfWork = null!;
+        private UserRolesController _controller = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockGenericUnitOfWork = new Mock<IGenericUnitOfWork<UserRol>>();
+            _mockUserRolUnitOfWork = new Mock<IUserRoleUnitOfWork>();
+            _controller = new UserRolesController(_mockGenericUnitOfWork.Object, _mockUserRolUnitOfWork.Object);
+        }
+
+        [TestMethod]
+        public async Task GetComboAsync_ReturnsOkObjectResult()
+        {
+            // Arrange
+            var comboData = new List<UserRol>
+            {
+                new UserRol {  Id = 1, Name = "Admin" }
+            };
+            _mockUserRolUnitOfWork.Setup(x => x.GetComboAsync()).ReturnsAsync(comboData);
+
+            // Act
+            var result = await _controller.GetComboAsync();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(comboData, okResult!.Value);
+            _mockUserRolUnitOfWork.Verify(x => x.GetComboAsync(), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_ReturnsOkObjectResult_WhenWasSuccessIsTrue()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var response = new Response<IEnumerable<UserRol>> { WasSuccess = true };
+            _mockUserRolUnitOfWork.Setup(x => x.GetAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(response.Result, okResult!.Value);
+            _mockUserRolUnitOfWork.Verify(x => x.GetAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_ReturnsBadRequestResult_WhenWasSuccessIsFalse()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var response = new Response<IEnumerable<UserRol>> { WasSuccess = false };
+            _mockUserRolUnitOfWork.Setup(x => x.GetAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+            _mockUserRolUnitOfWork.Verify(x => x.GetAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetPagesAsync_ReturnsOkObjectResult_WhenWasSuccessIsTrue()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var action = new Response<int> { WasSuccess = true, Result = 5 };
+            _mockUserRolUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagination)).ReturnsAsync(action);
+
+            // Act
+            var result = await _controller.GetPagesAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(action.Result, okResult!.Value);
+            _mockUserRolUnitOfWork.Verify(x => x.GetTotalPagesAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetPagesAsync_ReturnsBadRequestResult_WhenWasSuccessIsFalse()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var action = new Response<int> { WasSuccess = false };
+            _mockUserRolUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagination)).ReturnsAsync(action);
+
+            // Act
+            var result = await _controller.GetPagesAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+            _mockUserRolUnitOfWork.Verify(x => x.GetTotalPagesAsync(pagination), Times.Once());
+        }
+    }
+}
