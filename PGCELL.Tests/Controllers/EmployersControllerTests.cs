b8cdc33475d24930e4ffb51bbe9ac5564ce925e9
Sduran <sergioaduran93@gmail.com>
2023-11-22 13:14:09 -0500

    Test Unit Employer Controller

---
+++ PGCELL.Tests/Controllers/EmployersControllerTests.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PGCELL.Backend.Controllers;
+using PGCELL.Backend.UnitsOfWork;
+using PGCELL.Shared.DTOs;
+using PGCELL.Shared.Entites;
+using PGCELL.Shared.Responses;
+
+namespace PGCELL.Tests.Controllers
+{
+    [TestClass]
+    public class EmployersControllerTests
+    {
+        private Mock<IGenericUnitOfWork<Employer>> _mockGenericUnitOfWork = null!;
+        private Mock<IEmployerUnitOfWork> _mockEmployerUnitOfWork = null!;
+        private EmployersController _controller = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockGenericUnitOfWork = new Mock<IGenericUnitOfWork<Employer>>();
+            _mockEmployerUnitOfWork = new Mock<IEmployerUnitOfWork>();
+            _controller = new EmployersController(_mockGenericUnitOfWork.Object, _mockEmployerUnitOfWork.Object);
+        }
+
+        [TestMethod]
+        public async Task GetComboAsync_ReturnsOkObjectResult()
+        {
+            // Arrange
+            var comboData = new List<Employer>
+            {
+                new Employer {  Id = 1, Name = "Sergio" }
+            };
+            _mockEmployerUnitOfWork.Setup(x => x.GetComboAsync()).ReturnsAsync(comboData);
+
+            // Act
+            var result = await _controller.GetComboAsync();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(comboData, okResult!.Value);
+            _mockEmployerUnitOfWork.Verify(x => x.GetComboAsync(), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_ReturnsOkObjectResult_WhenWasSuccessIsTrue()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var response = new Response<IEnumerable<Employer>> { WasSuccess = true };
+            _mockEmployerUnitOfWork.Setup(x => x.GetAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(response.Result, okResult!.Value);
+            _mockEmployerUnitOfWork.Verify(x => x.GetAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_ReturnsBadRequestResult_WhenWasSuccessIsFalse()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var response = new Response<IEnumerable<Employer>> { WasSuccess = false };
+            _mockEmployerUnitOfWork.Setup(x => x.GetAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+            _mockEmployerUnitOfWork.Verify(x => x.GetAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetPagesAsync_ReturnsOkObjectResult_WhenWasSuccessIsTrue()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var action = new Response<int> { WasSuccess = true, Result = 5 };
+            _mockEmployerUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagination)).ReturnsAsync(action);
+
+            // Act
+            var result = await _controller.GetPagesAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(action.Result, okResult!.Value);
+            _mockEmployerUnitOfWork.Verify(x => x.GetTotalPagesAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetPagesAsync_ReturnsBadRequestResult_WhenWasSuccessIsFalse()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var action = new Response<int> { WasSuccess = false };
+            _mockEmployerUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagination)).ReturnsAsync(action);
+
+            // Act
+            var result = await _controller.GetPagesAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+            _mockEmployerUnitOfWork.Verify(x => x.GetTotalPagesAsync(pagination), Times.Once());
+        }
+    }
+}
